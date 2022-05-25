da95cebb7c6899aaf2065447867145f97f28d58b
Rietesh <rietesh4535@gmail.com>
2022-05-25 16:49:31 +0530

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -65,3 +65,6 @@ def upload_file():
       <input type=submit value=Upload>
     </form>
     '''
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
